8ad3f9cf9bf2e87f79b5a6729f7b2c946dd7b254
DargoDargonyx <dargodargonyx@outlook.com>
2025-04-12 19:11:48 -0400

    Added button animations

--- MissionEscapeGuy/Assets/Resources/Scripts/MainMenuScript.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/MainMenuScript.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 public class HelloWorldManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject hostButton;
+    [SerializeField] private GameObject joinButton;
+
     public void OnHostButtonClicked() 
     {
+        hostButton.GetComponent<Animator>().SetBool("buttonPressed", true);
         MasterController.isHost = true;
-        SceneManager.LoadSceneAsync("GameWorld");
+        Invoke("loadGameWorld", 0.5f);
     }
 
     public void OnClientButtonClicked() 
     {
+        joinButton.GetComponent<Animator>().SetBool("buttonPressed", true);
         MasterController.isHost = false;
+        Invoke("loadGameWorld", 0.5f);
+    }
+
+    private void loadGameWorld()
+    {
         SceneManager.LoadSceneAsync("GameWorld");
     }
+
 }
