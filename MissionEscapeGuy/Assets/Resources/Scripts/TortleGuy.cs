457be0987ab3667369251db77b45a79031a46dd0
Tencianity <arendechabert@gmail.com>
2025-04-12 15:56:54 -0400

    Update TortleGuy()

--- MissionEscapeGuy/Assets/Resources/Scripts/TortleGuy.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/TortleGuy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TortleGuy : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 2;
     private Rigidbody2D body;
     private Bullet bullet;
     private Vector2 currentPosition;
@@ -15,18 +15,19 @@ public class TortleGuy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        health = 4;
         body = body == null ? GetComponent<Rigidbody2D>() : body;
         bullet = bullet == null ? Resources.Load<Bullet>("Prefabs/Bullet") : bullet;
 
+        health = 4;
         currentPosition = transform.position;
         targetPosition = new(0, 0); // World Origin, where spaceship is located
+        targetDirection = currentPosition - targetPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.linearVelocity = targetDirection.normalized * moveSpeed;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
