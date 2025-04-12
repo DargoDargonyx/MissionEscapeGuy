efa5f943da5be120a12c81534d036740c47d20b4
Tencianity <arendechabert@gmail.com>
2025-04-12 12:57:32 -0400

    Update The Guy.cs

--- MissionEscapeGuy/Assets/Resources/Scripts/The Guy.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/The Guy.cs
@@ -7,14 +7,29 @@ public class TheGuy : MonoBehaviour
     private Rigidbody2D body;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+<<<<<<< Updated upstream
     //private Bullet bullet;
+=======
+    // private Bullet bullet;
+>>>>>>> Stashed changes
     private Vector2 moveDirection;
     private float moveX;
     private float moveY;
     private float moveSpeed;
     private bool isPurple;
     private bool isBlue;
-    private 
+    private bool isRed;
+    private bool isGreen;
+    private bool isOrange;
+    private const int MAX_HEALTH = 20;
+    private const int MAX_SHIELD = 10;
+    private int health;
+    private int shield;
+    [SerializeField] private Sprite purpleSprite;
+    [SerializeField] private Sprite blueSprite;
+    [SerializeField] private Sprite greenSprite;
+    [SerializeField] private Sprite orangeSprite;
+    [SerializeField] private Sprite blankSprite;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,5 +42,60 @@ public class TheGuy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FedUpdate()
+    {
+        
+    }
+
+    private void checkDirection()
+    {
+
+    }
+
+    private void fire()
+    {
+        
+    }
+
+    private void initializeColor()
+    {
+
+    }
+
+    public Vector2 getPosition()
+    {
+        return transform.position;
+    }
+
+    public Quaternion getRotation()
+    {
+        return transform.rotation;
+    }
+
+    public void takeDamage(int damage)
+    {
+
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void setHealth(int heatlh)
+    {
+        this.health = health;
+    }
+
+    public int getShield()
+    {
+        return shield;
+    }
+
+    public void setShield()
+    {
+        this.shield = shield;
     }
 }
