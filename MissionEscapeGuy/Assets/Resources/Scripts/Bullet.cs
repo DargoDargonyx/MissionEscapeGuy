7acd31baaa28ac16e19866fdf30526dba8a78936
Tencianity <arendechabert@gmail.com>
2025-04-12 15:16:05 -0400

    Update Bullet()

--- MissionEscapeGuy/Assets/Resources/Scripts/Bullet.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/Bullet.cs
@@ -8,25 +8,33 @@ public class Bullet : MonoBehaviour
     private float destroyDistance;
     private Vector2 initialPosition;
     private Vector2 currentPosition;
-    private Vector2 direction;
-    private Vector2 targetPosition;
+    private Vector2 direction = new(0,-1);
+    private Vector2 destroyPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        destroyDistance = 50f;
         bullet = bullet == null ? GetComponent<Rigidbody2D>() : bullet;
         initialPosition = currentPosition = bullet.transform.position;
+        destroyDistance = 50f;
+        
+        bullet.linearVelocity = direction;
+        destroyPosition = new Vector2(direction.x + destroyDistance, direction.y + destroyDistance);
     }
 
     // Update is called once per frame
     void Update()
     {
         currentPosition = bullet.transform.position;
+        if (currentPosition.x >= destroyPosition.x || currentPosition.y >= destroyPosition.y)
+        {
+            Destroy(gameObject, 0.3f);
+        }
     }
 
     public void setDirection(Vector2 direction)
     {
+        this.direction = direction;
         bullet.linearVelocity = direction.normalized;
     }
 }
