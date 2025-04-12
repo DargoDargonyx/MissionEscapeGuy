a8761040e765c1486fc7c47383708440e7fb493a
Tencianity <arendechabert@gmail.com>
2025-04-12 13:24:01 -0400

    Update Bullet.cs

--- MissionEscapeGuy/Assets/Resources/Scripts/Bullet.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/Bullet.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Transform parentTransform;
+    private Rigidbody2D bullet;
+    public float bulletSpeed;
+    private float destroyDistance;
+    private Vector2 initialPosition;
+    private Vector2 currentPosition;
+    private Vector2 direction;
+    private Vector2 targetPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,6 +19,11 @@ public class Bullet : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void setDirection()
     {
         
     }
