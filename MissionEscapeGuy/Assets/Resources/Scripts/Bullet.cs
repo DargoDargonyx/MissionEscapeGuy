d0cc300d978a5cabc86c94e0022bdcfc1993c62d
ultranine <jpdavis268@gmail.com>
2025-04-12 21:59:30 -0400

    Reapply "Fixed bullets"
    This reverts commit b805f2d4fe20725dd9e92ef9880f3ff8c3f9f661.

--- MissionEscapeGuy/Assets/Resources/Scripts/Bullet.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/Bullet.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Bullet : NetworkBehaviour
 {
-    private Transform parentTransform;
     private Rigidbody2D bullet;
-    public float bulletSpeed = 2.0f;
+    public float bulletSpeed = 10.0f;
     private int bulletDamage;
     private float destroyDistance;
     private Vector2 initialPosition;
@@ -19,12 +18,13 @@ public class Bullet : NetworkBehaviour
 
         bullet = bullet == null ? GetComponent<Rigidbody2D>() : bullet;
         initialPosition = currentPosition = bullet.transform.position;
-        destroyDistance = 50f;
+        destroyDistance = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position += transform.right * Time.deltaTime * bulletSpeed;
         currentPosition = bullet.transform.position;
         
         if (Math.Pow(currentPosition.x, 2) - Math.Pow(initialPosition.x, 2) >= Math.Pow(destroyDistance, 2) || Math.Pow(currentPosition.y, 2) - Math.Pow(initialPosition.y, 2) >= Math.Pow(destroyDistance, 2))
@@ -36,5 +36,10 @@ public class Bullet : NetworkBehaviour
     public void setDamage(int bulletDamage)
     {
         this.bulletDamage = bulletDamage;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject, 0.3f);
     }
 }
