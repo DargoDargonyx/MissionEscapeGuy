024f5afbc70bdc420f2530c1cdaff35a8d7bed37
ultranine <jpdavis268@gmail.com>
2025-04-13 00:48:46 -0400

    fafwa

--- MissionEscapeGuy/Assets/Resources/Scripts/ClientManager.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/ClientManager.cs
@@ -12,7 +12,7 @@ public class ClientManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
