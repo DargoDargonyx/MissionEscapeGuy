f2e26d35f511fdde079cfff943437ed53b1d7585
DargoDargonyx <dargodargonyx@outlook.com>
2025-04-13 10:32:37 -0400

    made buttons

--- MissionEscapeGuy/Assets/Resources/Scripts/MainMenuScript.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/MainMenuScript.cs
@@ -12,8 +12,61 @@ public class HelloWorldManager : MonoBehaviour
     [SerializeField] private GameObject hostButton;
     public void OnPlayButtonClicked() 
     {
-        hostButton.GetComponent<Animator>().SetBool("buttonPressed", true);
-        SceneManager.LoadSceneAsync("GameWorld");
+        if (buttonHasBeenPressed())
+        {
+            hostButton.GetComponent<Animator>().SetBool("buttonPressed", true);
+            SceneManager.LoadSceneAsync("GameWorld");
+        }
+    }
+
+    private bool buttonHasBeenPressed()
+    {
+        return playerData.isPurple 
+            && playerData.isBlue 
+            && playerData.isRed 
+            && playerData.isGreen 
+            && playerData.isOrange;
+    }
+
+    public void purpleButton()
+    {
+        playerData.isPurple = true;
+        playerData.isBlue = false;
+        playerData.isRed = false;
+        playerData.isGreen = false;
+        playerData.isOrange = false;
+    }
+    public void blueButton()
+    {
+        playerData.isPurple = false;
+        playerData.isBlue = true;
+        playerData.isRed = false;
+        playerData.isGreen = false;
+        playerData.isOrange = false;
+    }
+    public void redButton()
+    {
+        playerData.isPurple = false;
+        playerData.isBlue = false;
+        playerData.isRed = true;
+        playerData.isGreen = false;
+        playerData.isOrange = false;
+    }
+    public void greenButton()
+    {
+        playerData.isPurple = false;
+        playerData.isBlue = false;
+        playerData.isRed = false;
+        playerData.isGreen = true;
+        playerData.isOrange = false;
+    }
+    public void orangeButton()
+    {
+        playerData.isPurple = false;
+        playerData.isBlue = false;
+        playerData.isRed = false;
+        playerData.isGreen = false;
+        playerData.isOrange = true;
     }
 
 }
