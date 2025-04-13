90e92fa8e2678203fbd49c7f44ead8cca9bb2a3f
Tencianity <arendechabert@gmail.com>
2025-04-12 23:34:32 -0400

    MyPush

--- MissionEscapeGuy/Assets/Resources/Scripts/HealthManager.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/HealthManager.cs
@@ -1,16 +1,33 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
+    private Image image;
+    private SpriteResolver spriteResolver;
+    private NetworkObject networkObject;
+    private TheGuy player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameObject.GetComponent<NetworkObject>().Spawn();
+        player = player == null ? networkObject.GetComponent<TheGuy>() : player;
+
+        image = image == null ? GetComponent<Image>() : image;
+        spriteResolver = spriteResolver == null ? GetComponent<SpriteResolver>() : spriteResolver;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkHealth();
+    }
+
+    void checkHealth()
+    {
+        image.sprite = spriteResolver.spriteLibrary.GetSprite("GUI", "HealthBar_" + player.getHealth());
     }
 }
--- MissionEscapeGuy/Assets/Resources/Scripts/ShieldManager.cs
+++ MissionEscapeGuy/Assets/Resources/Scripts/ShieldManager.cs
@@ -1,16 +1,30 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
+using UnityEngine.UI;
 
 public class ShieldManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Image image;
+    private SpriteResolver spriteResolver;
+    private NetworkObject networkObject;
+    private TheGuy player;
     void Start()
     {
-        
+        player = player == null ? networkObject.GetComponent<TheGuy>() : player;
+
+        image = image == null ? GetComponent<Image>() : image;
+        spriteResolver = spriteResolver == null ? GetComponent<SpriteResolver>() : spriteResolver;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkShield();
+    }
+
+    void checkShield()
+    {
+        image.sprite = spriteResolver.spriteLibrary.GetSprite("GUI", "ShieldBar_" + player.getShield());
     }
 }
